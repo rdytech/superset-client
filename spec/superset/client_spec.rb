9c78be1aafc36cde0f96499fcdff8cdce459f2f1
jbat <jonathon.batson@gmail.com>
2023-11-26 09:41:00 +1100

    adjust gemspec

---
+++ spec/superset/client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Superset::Client do
+  it "has a version number" do
+    expect(Superset::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
