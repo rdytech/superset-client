2b54d96fc88597a32a31b55700b1001ecd3af7da
jbat <jonathon.batson@gmail.com>
2023-12-05 16:27:08 +1100

    setup gem folder structure

---
+++ lib/superset.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "superset/version"
+
+module Superset
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/superset_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Superset do
+  it "has a version number" do
+    expect(Superset::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
