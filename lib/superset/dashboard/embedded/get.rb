b3cb9635d325322a9db9e3adbfe2594de04b241d
jbat <jonathon.batson@gmail.com>
2023-12-08 10:28:07 +1100

    add dashboard/embedded endpoint

---
+++ lib/superset/dashboard/embedded/get.rb
@@ -0,0 +1,57 @@
+module Superset
+  module Dashboard
+    module Embedded
+      class Get < Superset::Request
+        attr_reader :id  # dashboard id
+
+        def self.call(id)
+          self.new(id).list
+        end
+
+        def initialize(id)
+          @id = id
+        end
+
+        def response
+          @response ||= client.get(route)
+        rescue Happi::Error::NotFound => e
+          puts "Dashboard #{id} has no Embedded settings. (skipping)"  # some dashboards don't have embedded settings, fine to ignore.
+          @response = { result: [] }.with_indifferent_access
+          @response
+        end
+
+        def result
+          response[:result].empty? ? [] : [ super ] # wrap single result in an array so it can be used in the tt list and table
+        end
+
+        def allowed_domains
+          result.first['allowed_domains'] unless response[:result].empty?
+        end
+
+        def uuid
+          result.first['uuid'] unless response[:result].empty?
+        end
+
+        def list
+          super unless response[:result].empty?
+        end
+
+        private
+
+        def route
+          "dashboard/#{id}/embedded"
+        end
+
+        def list_attributes
+          [:dashboard_id, :uuid, :allowed_domains, :changed_on]
+        end
+
+        # when displaying embedded details, show dashboard title as well
+        def title
+          Superset::Dashboard::Get.new(id).title
+        end
+      end
+    end
+  end
+end
+
---
+++ spec/superset/dashboard/embedded/get_spec.rb
@@ -0,0 +1,98 @@
+require 'spec_helper'
+
+RSpec.describe Superset::Dashboard::Embedded::Get, type: :service do
+  subject { described_class.new(dashboard_id) }
+  let(:dashboard_id) { 1 }
+
+  describe 'with a dashboard that has embedded settings, ie has a result' do
+    let(:allowed_domains) { ['http://jobready.127.0.0.1.nip.io:9292/'] }
+    let(:uuid) { '631bxxxx-xxxx-xxxx-xxxx-xxxx7f221247' }
+    let(:response) do
+      {
+        'result' =>
+          {
+            "allowed_domains" => allowed_domains,
+            "changed_on"      => "2023-10-30T03:06:51.437527",
+            "dashboard_id"    => "1",
+            "uuid"            => uuid
+          }.with_indifferent_access
+      }.with_indifferent_access
+    end
+
+    before do
+      allow(subject).to receive(:response).and_return(response)
+      allow(subject).to receive(:title).and_return('1: Test Dashboard')
+    end
+
+    describe '#allowed_domains' do
+      it 'returns a list of allowed domains' do
+        expect(subject.allowed_domains).to eq(allowed_domains)
+      end
+    end
+
+    describe '#uuid' do
+      it 'returns uuid' do
+        expect(subject.uuid).to eq(uuid)
+      end
+    end
+
+    describe '#table' do
+      it 'prints a table with the dashboard title and charts' do
+          expect(subject.table.to_s).to eq(
+            "+-----------+--------------------------------------+--------------------------------------------+----------------------------+\n" \
+            "|                                                     1: Test Dashboard                                                      |\n" \
+            "+-----------+--------------------------------------+--------------------------------------------+----------------------------+\n" \
+            "| Dashboard | Uuid                                 | Allowed domains                            | Changed on                 |\n" \
+            "+-----------+--------------------------------------+--------------------------------------------+----------------------------+\n" \
+            "| 1         | 631bxxxx-xxxx-xxxx-xxxx-xxxx7f221247 | [\"http://jobready.127.0.0.1.nip.io:9292/\"] | 2023-10-30T03:06:51.437527 |\n" \
+            "+-----------+--------------------------------------+--------------------------------------------+----------------------------+" \
+          )
+      end
+    end
+  end
+
+  describe 'with a dashboard that DOES NOT have any embedded settings' do
+    before do
+      allow(subject).to receive(:title).and_return('1: Test Dashboard')
+      allow(subject).to receive(:client).and_raise(Happi::Error::NotFound)
+    end
+
+    describe '#response' do
+      specify do
+        expect(subject.response).to eq({ 'result' => [] })
+      end
+    end
+
+    describe '#result' do
+      specify 'is an empty array' do
+        expect(subject.result).to eq([])
+      end
+    end
+
+    describe '#allowed_domains' do
+      it 'returns a list of allowed domains' do
+        expect(subject.allowed_domains).to eq(nil)
+      end
+    end
+
+    describe '#uuid' do
+      it 'returns uuid' do
+        expect(subject.uuid).to eq(nil)
+      end
+    end
+
+    describe '#table' do
+      it 'prints a table with zero rows' do
+          expect(subject.table.to_s).to eq(
+            "+-----------+------+-----------------+------------+\n" \
+            "|                1: Test Dashboard                |\n" \
+            "+-----------+------+-----------------+------------+\n" \
+            "| Dashboard | Uuid | Allowed domains | Changed on |\n" \
+            "+-----------+------+-----------------+------------+\n" \
+            "+-----------+------+-----------------+------------+"
+          )
+      end
+    end
+  end
+end
+
