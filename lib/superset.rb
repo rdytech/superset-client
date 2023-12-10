fac657a438977fd0e1128e5ba0de4caabe251d4b
jbat <jonathon.batson@gmail.com>
2023-12-11 08:37:23 +1000

    adjust requires

--- lib/superset.rb
+++ lib/superset.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 require 'require_all'
+
 require_rel "superset/credential"
 require_relative "superset/authenticator"
 require_relative "superset/client"
 require_relative "superset/display"
 require_relative "superset/request"
-require_all 'lib'
+
+require_rel "superset"
 
 module Superset
   class Error < StandardError; end
