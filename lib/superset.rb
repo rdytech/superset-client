efa5fcd98633f97232b21606cc1a1f9d9bfd540b
jbat <jonathon.batson@gmail.com>
2023-12-11 08:02:08 +1000

    add require-all gem with usage

--- lib/superset.rb
+++ lib/superset.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
-require_relative "superset/version"
-require_relative "superset/credential/api_user"
-require_relative "superset/credential/embedded_user"
+require_all "lib/superset/credential"
 require_relative "superset/authenticator"
 require_relative "superset/client"
 require_relative "superset/display"
 require_relative "superset/request"
-require_relative "superset/dashboard/list"
+require_all 'lib'
 
 module Superset
   class Error < StandardError; end
--- superset.gemspec
+++ superset.gemspec
@@ -39,6 +39,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency "terminal-table", "~> 1.8"
   spec.add_dependency "rake", "~> 13.0"
   spec.add_dependency "rollbar", "~> 3.4"
+  spec.add_dependency "require_all", "~> 3.0"
 
   spec.add_development_dependency "rspec", "~> 3.0"
   spec.add_development_dependency "rubocop", "~> 1.5"
