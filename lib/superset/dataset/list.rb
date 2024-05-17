65282f678a89c29a6a9cc6bf51a9694fd4e2430f
jbat <jonathon.batson@gmail.com>
2024-05-17 13:51:08 +1000

    AC-10426 Adds endpoint for dataset creation, route info

--- lib/superset/dataset/list.rb
+++ lib/superset/dataset/list.rb
@@ -1,12 +1,13 @@
 module Superset
   module Dataset
     class List < Superset::Request
-      attr_reader :title_contains, :title_equals, :schema_equals
+      attr_reader :title_contains, :title_equals, :schema_equals, :database_id_eq
 
-      def initialize(page_num: 0, title_contains: '', title_equals: '', schema_equals: '')
+      def initialize(page_num: 0, title_contains: '', title_equals: '', schema_equals: '', database_id_eq: '')
         @title_contains = title_contains
         @title_equals = title_equals
         @schema_equals = schema_equals
+        @database_id_eq = database_id_eq
         super(page_num: page_num)
       end
 
@@ -26,6 +27,7 @@ module Superset
         filters << "(col:table_name,opr:ct,value:'#{title_contains}')" if title_contains.present?
         filters << "(col:table_name,opr:eq,value:'#{title_equals}')" if title_equals.present?
         filters << "(col:schema,opr:eq,value:'#{schema_equals}')" if schema_equals.present?
+        filters << "(col:database,opr:rel_o_m,value:#{database_id_eq})" if database_id_eq.present? # rel one to many
         unless filters.empty?
           "filters:!(" + filters.join(',') + "),"
         end
---
+++ lib/superset/route_info.rb
@@ -0,0 +1,34 @@
+module Superset
+  class RouteInfo < Superset::Request
+    alias result response
+
+    attr_reader :route
+    
+    def initialize(route:)
+      @route = route
+    end
+
+    def perform
+      validate_route
+      response
+    end
+
+    def response
+      validate_route
+      @response ||= client.get(route)
+    end
+
+    def filters
+      result['filters']
+    end
+
+    private
+
+    def validate_route
+      unless route.present? && route.is_a?(String)
+        puts "Example Route: 'dashboard/_info' "
+        raise "Error: route string is required" unless route.present? && route.is_a?(String)
+      end
+    end
+  end
+end
