db5da87f6369dd70521b757828758cba4fd13ac9
jbat <jonathon.batson@gmail.com>
2024-09-10 09:40:47 +1000

    correct spec

--- spec/superset/services/import_dashboard_to_env_spec.rb
+++ spec/superset/services/import_dashboard_to_env_spec.rb
@@ -10,14 +10,19 @@ RSpec.describe Superset::Services::ImportDashboardToEnv do
     )
   }
 
-  let(:dashboard_export_zip_file) { 'spec/fixtures/dashboard_18_export_20240322.zip' }
   # fixture export zip file database.yaml has database_name: examples, and uuid: a2dc77af-e654-49bb-b321-40f6b559a1ee
+  let(:dashboard_export_zip_file) { 'spec/fixtures/dashboard_18_export_20240322.zip' }
 
+  # fixture target_database_yaml has database_name: acme_client, and uuid: 2f4e4aaf-0c44-4845-bc83-de4c4d2ae20f
   let(:target_database_yaml) { 'spec/fixtures/acme_client.yaml' }
   let(:target_database_yaml_data) { YAML.load_file(subject.target_database_yaml) }
-  # fixture target_database_yaml has database_name: acme_client, and uuid: 2f4e4aaf-0c44-4845-bc83-de4c4d2ae20f
 
   let(:target_database_schema) { 'megacorp' }
+
+  before do
+    allow(subject).to receive(:import_new_dashboard_config) { true }
+    allow(subject).to receive(:imported_dashboard_details) { { dashboard_id: 1, dashboard_title: 'Test Dashboard' } }
+  end
 
   describe '#perform' do
     it 'creates a new dashboard import zip file' do
@@ -25,7 +30,7 @@ RSpec.describe Superset::Services::ImportDashboardToEnv do
       subject.perform
       expect(File.exist?(subject.send(:new_zip_file))).to be true
     end
-    
+
     context 'confirming new database yaml file and contents' do
       #let(:expected_new_database_schema)  { 'megacorp' }
       let(:expected_new_database_name)    { 'acme_client' }
@@ -36,21 +41,21 @@ RSpec.describe Superset::Services::ImportDashboardToEnv do
         # confirm target database yaml settings
         expect(target_database_yaml_data['database_name']).to eq(expected_new_database_name)
         expect(target_database_yaml_data['uuid']).to eq(expected_new_database_uuid)
-        
+
         # confirm current database yaml settings in the current export zip file
         current_database_files = subject.extracted_files.select {|f| f.include?('databases') }
         current_database_file_yaml = YAML.load_file(current_database_files.first)
         expect(current_database_file_yaml['database_name']).to eq('examples')
         expect(current_database_file_yaml['uuid']).to eq('a2dc77af-e654-49bb-b321-40f6b559a1ee')
-        
+
         # get the previous database yaml file and UUID
-        
+
         subject.perform
 
         # get all files in the new zip root path
         new_export_files = Dir[File.join(subject.send(:dashboard_export_root_path), '**', '**')]
         new_database_file = new_export_files.select {|f| f.include?('acme_client.yaml') }
-        
+
         # confirm the new database yaml file exists in the new zip root path
         expect(new_export_files.select {|f| f.include?('examples.yaml') }.any?).to be false
         expect(new_database_file.count).to eq 1
@@ -64,7 +69,7 @@ RSpec.describe Superset::Services::ImportDashboardToEnv do
     end
 
     context 'confirming all dataset yaml files are updated' do
-      # wip
+      # wip .. more specs coming
     end
   end
 end
