67f1d386f702e0a69ac64a136796366e2fa66108
jbat <jonathon.batson@gmail.com>
2025-05-22 09:03:36 +1000

    add spec for client call and memoization

--- spec/superset/dataset/refresh_spec.rb
+++ spec/superset/dataset/refresh_spec.rb
@@ -4,9 +4,10 @@ RSpec.describe Superset::Dataset::Refresh do
   subject { described_class.new(id) }
   let(:id) { 1 }
   let(:result) { { "message": "OK" } }
+  let(:client) { instance_double(Superset::Client) }
 
   before do
-    allow(subject).to receive(:response).and_return(result)
+    allow(subject).to receive(:client).and_return(client)
   end
 
   describe '.call' do
@@ -18,7 +19,15 @@ RSpec.describe Superset::Dataset::Refresh do
 
   describe '#perform' do
     specify do
+      expect(client).to receive(:put).with("dataset/#{id}/refresh").and_return(result)
       expect(subject.perform).to eq result
+    end
+  end
+
+  describe '#response' do
+    specify 'memoizes the response' do
+      expect(client).to receive(:put).with("dataset/#{id}/refresh").once.and_return(result)
+      2.times { subject.response }
     end
   end
 end
