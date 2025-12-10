a4009ea3b31de49d9a4f4a492c3ac45897b36d1a
jbat <jonathon.batson@gmail.com>
2025-12-10 16:57:48 +1100

    add endpoint for database connection info

---
+++ spec/superset/database/connection_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+RSpec.describe Superset::Database::Connection do
+  subject { described_class.new(id) }
+  let(:id) { 111 }
+  let(:result) do
+    {
+      "backend" => "postgresql",
+      "database_name" => "Acme Client Staging",
+      "driver" => "psycopg2",
+      "id" => 21,
+      "parameters" => {
+        "database" => "acme_staging",  
+        "host" => "some.host.com",
+      }
+    }
+  end
+
+  before do
+    allow(subject).to receive(:result).and_return(result)
+  end
+
+  describe '.call' do
+    specify do
+      expect_any_instance_of(described_class).to receive(:result)
+      described_class.call(id)
+    end
+  end
+
+  describe '#connection_db_name' do
+    specify do
+      expect(subject.connection_db_name).to eq "acme_staging"
+    end
+  end
+end
