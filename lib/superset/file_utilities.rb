070ee6a72b59507b69e968b990be4ce1af0c7577
jbat <jonathon.batson@gmail.com>
2026-02-23 08:50:32 +1100

    remove rubyzip upgrade comment

--- lib/superset/file_utilities.rb
+++ lib/superset/file_utilities.rb
@@ -12,8 +12,6 @@ module Superset
           entries << entry_path
           FileUtils.mkdir_p(File.dirname(entry_path))
 
-          # RubyZip 3.x: destination_directory must allow our absolute entry_path.
-          # Default '.' makes extract_path fail start_with?(dest_dir) and skip extraction.
           zip.extract(entry, entry_path, destination_directory: '/') unless File.exist?(entry_path)
         rescue => e
           raise "Error extracting file #{entry.name}: #{e.message}"
