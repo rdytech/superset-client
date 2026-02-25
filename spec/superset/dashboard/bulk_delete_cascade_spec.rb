ff9c0aee687d602dfe0df0061500ea7677b67647
jbat <jonathon.batson@gmail.com>
2026-02-25 17:05:25 +1100

    fix delete spec

--- spec/superset/dashboard/bulk_delete_cascade_spec.rb
+++ spec/superset/dashboard/bulk_delete_cascade_spec.rb
@@ -1,8 +1,16 @@
 require 'spec_helper'
 
 RSpec.describe Superset::Dashboard::BulkDeleteCascade do
-  subject { described_class.new(dashboard_ids: dashboard_ids) }
+  subject { described_class.new(dashboard_ids: dashboard_ids, dry_run: dry_run) }
   let(:dashboard_ids) { nil }
+  let(:dry_run) { true }
+
+  describe '#dry_run' do
+    it 'defaults to true when not passed' do
+      instance = described_class.new(dashboard_ids: [1])
+      expect(instance.dry_run).to eq(true)
+    end
+  end
 
   describe '.perform' do
     context 'when dashboard_ids is not present' do
@@ -21,19 +29,46 @@ RSpec.describe Superset::Dashboard::BulkDeleteCascade do
 
     context 'when dashboard_ids are valid' do
       let(:dashboard_ids) { [1] }
+      let(:datasets_list) { double('Datasets::List', ids: [11, 12]) }
+      let(:charts_list) { double('Charts::List', chart_ids: [21, 22]) }
 
       before do
-        allow(Superset::Dashboard::Datasets::List).to receive(:new).with(dashboard_id: 1).and_return(double(datasets_details: [{ id: 11 }, { id: 12 }]))
-        allow(Superset::Dataset::BulkDelete).to receive(:new).with(dataset_ids: [11,12]).and_return(double(perform: true))
-
-        allow(Superset::Dashboard::Charts::List).to receive(:new).with(1).and_return(double(chart_ids: [21, 22]))
-        allow(Superset::Chart::BulkDelete).to receive(:new).with(chart_ids: [21, 22]).and_return(double(perform: true))
-
-        allow(Superset::Dashboard::Delete).to receive(:new).with(dashboard_id: 1, confirm_zero_charts: true).and_return(double(perform: true))
+        allow(Superset::Dashboard::Datasets::List).to receive(:new).with(dashboard_id: 1).and_return(datasets_list)
+        allow(Superset::Dashboard::Charts::List).to receive(:new).with(1).and_return(charts_list)
       end
 
-      it 'bulk deletes related charts, datasets and the dashboard' do
-        expect(subject.perform).to eq(true)
+      context 'when dry_run is true (default)' do
+        let(:dry_run) { true }
+
+        it 'returns true without calling delete services' do
+          expect(Superset::Dataset::BulkDelete).not_to receive(:new)
+          expect(Superset::Chart::BulkDelete).not_to receive(:new)
+          expect(Superset::Dashboard::Delete).not_to receive(:new)
+
+          expect(subject.perform).to eq(true)
+        end
+      end
+
+      context 'when dry_run is false' do
+        let(:dry_run) { false }
+
+        before do
+          allow(Superset::Dataset::BulkDelete).to receive(:new).with(dataset_ids: [11, 12]).and_return(double(perform: true))
+          allow(Superset::Chart::BulkDelete).to receive(:new).with(chart_ids: [21, 22]).and_return(double(perform: true))
+          allow(Superset::Dashboard::Delete).to receive(:new).with(dashboard_id: 1, confirm_zero_charts: true).and_return(double(perform: true))
+        end
+
+        it 'bulk deletes related charts, datasets and the dashboard' do
+          expect(subject.perform).to eq(true)
+        end
+
+        it 'calls Dataset::BulkDelete, Chart::BulkDelete and Dashboard::Delete' do
+          subject.perform
+
+          expect(Superset::Dataset::BulkDelete).to have_received(:new).with(dataset_ids: [11, 12])
+          expect(Superset::Chart::BulkDelete).to have_received(:new).with(chart_ids: [21, 22])
+          expect(Superset::Dashboard::Delete).to have_received(:new).with(dashboard_id: 1, confirm_zero_charts: true)
+        end
       end
     end
   end
