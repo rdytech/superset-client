7e60a594a0b194ed1bd607f1879f93e40b797c37
jbat <jonathon.batson@gmail.com>
2026-07-21 13:26:31 +1000

    Route Superset::Logger through configurable Superset.logger

--- lib/superset/logger.rb
+++ lib/superset/logger.rb
@@ -1,20 +1,11 @@
 module Superset
   class Logger
-
     def info(msg)
-      # puts msg   # allow logs to console
-      logger.info msg
+      Superset.logger.info(msg)
     end
 
     def error(msg)
-      # puts msg   # allow logs to console
-      logger.error msg
-    end
-
-    def logger
-      @logger ||= begin
-        ::Logger.new("log/superset-client.log")
-      end
+      Superset.logger.error(msg)
     end
   end
 end
